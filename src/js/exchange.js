a30e549db9562cf620d60b92a83eacdfe82bd6e0
Samuel Majerus <ladolego@gmail.com>
2022-09-09 10:15:39 -0700

    Updates most of the UI- and API Call Service- Logic code components included in Template.    Applied a trick or two (learned from working on the Crypto-API 2-day project these past couple days) to both the HTML and UI Logic.   Also moved API Call Service Logic into a separate JS file. (Created 'js' subdirectory in 'src' for both JS files). [WIP:  UI Logic, API Call Service Logic]

---
+++ src/js/exchange.js
@@ -0,0 +1,17 @@
+// Business Logic
+export default function getConvRate(target) {  //'target' currency for conversion (the 'to' in "from -> to") is passed into fn. 
+  let request = new XMLHttpRequest();
+  const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${target}`; 
+
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElements(response, target); 
+    } else { 
+      printError(this, response, target); 
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,49 +1,34 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-
-// Business Logic
-
-function getWeather(city) {
-  let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
-  request.addEventListener("loadend", function() {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response, city);
-    } else {
-      printError(this, response, city);
-    }
-  });
-
-  request.open("GET", url, true);
-  request.send();
-}
+import './exchange.js'; 
 
 // UI Logic
-
-function printError(request, apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+function printError(request, apiResponse, tgt) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the conversion rate for ${tgt}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
 }
 
-function printElements(apiResponse, city) { 
+function printElements(apiResponse, tgt) { 
+  let convertedAmt; 
   document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
   The temperature in Fahrenheit is ${(1.8 * (apiResponse.main.temp-273))+32} degrees.
   The temperature in Kelvin is ${apiResponse.main.temp} degrees.
   The timezone is ${apiResponse.timezone}.
   The tempurature feels like ${apiResponse.main.feels_like} degrees Kelvin.
   Sunrise will occur at ${apiResponse.sys.sunrise} UTC.
-  Sunset will occur at ${apiResponse.sys.sunset} UTC.`; 
+  Sunset will occur at ${apiResponse.sys.sunset} UTC.`;  
 }
 
 function handleFormSubmission(event) {
-  event.preventDefault();
-  const city = document.querySelector('#location').value;
-  document.querySelector('#location').value = null;
-  getWeather(city);
+  event.preventDefault(); 
+  const amtInput = document.querySelector('input#usd-amt-input').value; 
+  const convertTo = document.querySelector('select#tgt-for-conv').value; 
+  document.querySelector('input#usd-amt-input').value = null; 
+  document.querySelector('select#tgt-for-conv').value = "0"; 
+  document.querySelector('div#input-amt-storage').innerHTML = amtInput; //Stores the USD-amount input by user, to access later. 
+  getConvRate(convertTo); 
 }
 
 window.addEventListener("load", function() {
-  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+  document.querySelector('form#user-input').addEventListener("submit", handleFormSubmission);
 });
