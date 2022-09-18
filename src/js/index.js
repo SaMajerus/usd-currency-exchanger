6ab12d6f46fc3f2685ec854057ee4f015a3d9994
Samuel Majerus <ladolego@gmail.com>
2022-09-18 13:19:11 -0700

    Changes type of input field in HTML to Number.   Adds error checking for if the input value is not a Number

--- src/js/index.js
+++ src/js/index.js
@@ -7,12 +7,19 @@ import {getConvRate} from './exchange.js';
 function handleFormSubmission(event) {
   event.preventDefault(); 
   document.querySelector('#showResult').innerText = null;  //Clears the response section (in case anything is there from a previous event.)
-  const amtInput = document.querySelector('input#usd-amt-input').value; 
+  
+  //Storage vars for input values
+  const amtInput = document.querySelector('input#usd-amt-input').value;  //The value entered by the user (presumably a numerical amount in USD). 
   const convertTo = document.querySelector('select#tgt-for-conv').value; 
-  if(!(convertTo === "0")){
-    document.querySelector('input#usd-amt-input').value = null; 
-    document.querySelector('select#tgt-for-conv').value = "0";  
-    getConvRate(convertTo, amtInput);  
+  
+  if (!(convertTo === "0")){
+    if (typeof amtInput){
+      document.querySelector('input#usd-amt-input').value = null; 
+      document.querySelector('select#tgt-for-conv').value = "0";  
+      getConvRate(convertTo, amtInput);  
+    }else{
+      document.querySelector("#showResult").innerText = "Invalid input for USD value. Please try again.";  
+    }
   } else {
     document.querySelector("#showResult").innerText = "Invalid choice for currency. Please try again."; 
   }
